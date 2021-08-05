9235bd78f5bb45d318d0b5a1c3ed7ded8c28b4aa
test <elpsi7@naver.com>
2021-08-06 00:44:18 +0900

    3.5 JPA Auditing으로 생성시간/수정시간 자동화하기

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,7 +24,7 @@ import javax.persistence.Id;
 // 기본값으로 크래스의 카멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭
 // ex) SalesManger.java -> sales_mananger table
 @Entity
-public class Posts{
+public class Posts extends BaseTimeEntity {
 
     // 2. @Id -> 해당 테이블의 PK 필드를 나타냅니다.
     @Id
